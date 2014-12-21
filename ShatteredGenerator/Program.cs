21bdf268a132ceeff84c18393b59ea5ab7d7e17d
AFishyFez <fishnchips65@gmail.com>
2014-12-21 12:43:38 -0800

    Country religion matches province religion
    Made the religion of the country match the province. Did the same thing for culture, but that didn't seem to fix the nation's primary culture. My guess is that the culture is being overwrote as the history of the province is processed. Will have to investigate further.

--- ShatteredGenerator/Program.cs
+++ ShatteredGenerator/Program.cs
@@ -120,6 +120,13 @@ namespace ShatteredGenerator
 				newCountry.ClearHistory();
 				var newCountryHistory = oldCountryHistory.Clone();
 				newCountryHistory.ClearHistory();
+				
+				//Make the culture and religion match the province
+		                if(provinceFile.Value.Culture != null)
+		                    newCountryHistory.Culture = provinceFile.Value.Culture;
+		
+		                if (provinceFile.Value.Religion != null)
+		                    newCountryHistory.Religion = provinceFile.Value.Religion;
 
 				// Give our new country a new shiny flag
 				newCountry.Color = new Eu4Color(
